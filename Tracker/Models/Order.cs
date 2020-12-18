08700ceea81e40a3dd55ef06a496301509b8e615
Chelsea Becker <cschweig2@gmail.com>
2020-12-18 10:22:15 -0800

    remove commenting

--- Tracker/Models/Order.cs
+++ Tracker/Models/Order.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 
-// Order class should include properties for the title, description, price, date and any other properties I would like to include
 namespace Tracker.Models
 {
     public class Order
