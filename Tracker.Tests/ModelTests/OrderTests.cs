aa36587575185b0c7bd37d0793fb4aba63e48864
Chelsea Becker <cschweig2@gmail.com>
2020-12-18 09:08:00 -0800

    test 3 FAIL

--- Tracker.Tests/ModelTests/OrderTests.cs
+++ Tracker.Tests/ModelTests/OrderTests.cs
@@ -26,6 +26,18 @@ namespace Tracker.Tests
             string result = newOrder.Title;
             Assert.AreEqual(title, result);
         }
+        [TestMethod]
+        public void SetTitle_SetsTitleOfOrder_String()
+        {
+            string title = "Patty's Pastry Order";
+            Order newOrder = new Order(title, "description", 25, 12182020);
+            string newTitle = "Justin's Pastry Order";
+            string otherNewTitle = "Test Title";
+            newOrder.Title = otherNewTitle;
+            string result = newOrder.Title;
+            Assert.AreEqual(newTitle, result);
+        }
+
 
     }
 }
