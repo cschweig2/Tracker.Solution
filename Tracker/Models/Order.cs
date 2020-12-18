cc885e010711fef191bf568c8d9865f57ef64e7e
Chelsea Becker <cschweig2@gmail.com>
2020-12-18 08:23:24 -0800

    add order and vendor classes

---
+++ Tracker/Models/Order.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+// Order class should include properties for the title, description, price, date and any other properties I would like to include
+namespace Tracker.Models
+{
+    public class Order
+    {
+    }
+}
