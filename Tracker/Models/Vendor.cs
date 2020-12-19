1b7023b16fc7576823f22bbcf398097cb507e165
Chelsea Becker <cschweig2@gmail.com>
2020-12-18 16:50:19 -0800

    remove comments

--- Tracker/Models/Vendor.cs
+++ Tracker/Models/Vendor.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 
-// vendor class should include properties for the vendor's name, description of the vendor
-// a List of Order's belonging to the vendor, and any other properties
 namespace Tracker.Models
 {
     public class Vendor
